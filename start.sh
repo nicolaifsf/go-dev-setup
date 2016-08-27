98aa946c4182e46e92ece3b3ab93573b5ddc7769
Nicolai <nicolaifsf@gmail.com>
2016-08-27 16:38:53 -0700

    Bugfix. Remove the eval on the command

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
-if !docker -v; then
+docker -v
+if [[ $? != 0 ]]; then
     echo "Please install/start docker first!"
 else
     docker build . -t golang-docker-dev-setup
-    echo "alias gg=\"docker run -it -v $PWD:/go/src golang-docker-dev-setup\"" >> ~/.zshrc
+    echo 'alias gg="docker run -it -v $PWD:/go/src golang-docker-dev-setup"' >> ~/.zshrc
 fi
 echo "Completed, please restart your terminal"
